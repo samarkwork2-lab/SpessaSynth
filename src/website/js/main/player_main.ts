c22be3444dcaba1151156b385f35ffc0caf9489a
samark.work2 <samark.work2@gmail.com>
2025-12-09 15:57:52 +0000

    fixed

---
+++ src/website/js/main/player_main.ts
@@ -0,0 +1,34 @@
+import { Manager } from "../manager/manager.js";
+import { BasicSoundBank } from "spessasynth_core";
+
+const SAMPLE_RATE = 44100;
+
+async function initialize() {
+    const context = new AudioContext({ sampleRate: SAMPLE_RATE });
+    const soundFontBuffer = await BasicSoundBank.getSampleSoundBankFile();
+    const manager = new Manager(context, soundFontBuffer);
+    await manager.ready;
+
+    const fileInput = document.getElementById("midi_file_input");
+    const sfInput = document.getElementById("sf_file_input");
+
+    fileInput.onchange = async () => {
+        if (fileInput.files.length > 0) {
+            const files = Array.from(fileInput.files).map(async file => ({
+                binary: await file.arrayBuffer(),
+                fileName: file.name
+            }));
+            const parsed = await Promise.all(files);
+            manager.play(parsed);
+        }
+    };
+
+    sfInput.onchange = async () => {
+        if (sfInput.files.length > 0) {
+            const sfBuffer = await sfInput.files[0].arrayBuffer();
+            await manager.reloadSf(sfBuffer);
+        }
+    };
+}
+
+initialize();
